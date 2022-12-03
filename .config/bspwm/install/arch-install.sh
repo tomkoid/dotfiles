70160b7393992273d6fb1390ceac233830dbf5a0
Tomkoid <tomkoid@tomkoid.tk>
2022-12-03 15:00:15 +0100

    .config/bspwm/install/arch-install.sh: Added a few dependencies

--- .config/bspwm/install/arch-install.sh
+++ .config/bspwm/install/arch-install.sh
@@ -7,4 +7,5 @@ echo "### INSTALL NEEDED FONTS"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip
 unzip /tmp/JetBrainsMono.zip -d /tmp/
 rm /tmp/JetBrainsMono.zip
+rm /tmp/readme.md
 sudo mv /tmp/JetBrains* /usr/share/fonts/TTF/
