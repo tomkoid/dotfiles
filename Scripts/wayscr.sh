1b9cf58582379921a6ffb66cce767eb8dd8a34b9
Tomkoid <tomkoid@tomkoid.tk>
2023-12-25 15:07:47 +0100

    scripts: use hyprshot instead of grim

--- Scripts/wayscr.sh
+++ Scripts/wayscr.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+kitty -e "Scripts/wayscr-prompt.sh"
+
+screenshot_type=$(cat /tmp/screenshot_type)
+
 # grim -g "$(slurp)" - | /usr/bin/wl-copy
-grim -g $(slurp) - | wl-copy
-# hyprshot -m region -r | wl-copy
+# grim -g $(slurp) - | wl-copy
+# grim -g $(slurp) - | wl-copy
+hyprshot -m ${screenshot_type} -r | wl-copy
