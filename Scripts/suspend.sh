a11a9dff01239aa7a985c365521471497dd64c9f
Tomkoid <tomaszierl@outlook.com>
2024-03-31 14:06:54 +0200

    suspend.sh: improve suspending script, lock before suspend
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- Scripts/suspend.sh
+++ Scripts/suspend.sh
@@ -2,7 +2,27 @@
 
 output=$(playerctl status -a | grep "Playing")
 
+lock_command="hyprlock"
+suspend_command="systemctl suspend"
+
 # Suspend only if nothing is playing
 if [ -z "$output" ]; then
-  systemctl suspend
+  # Stop any currently running mpc
+  mpc stop
+
+  # Lock before suspending
+  setsid -f $lock_command &>/dev/null
+
+  sleep 10
+
+  if [[ $(cat /tmp/idle) == "idle" ]]
+  then
+    $suspend_command 
+
+    # if still idle after suspending, suspend
+    if [[ $(cat /tmp/idle) == "idle" ]]
+    then
+      $suspend_command
+    fi
+  fi
 fi
