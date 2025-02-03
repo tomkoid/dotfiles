457fdfe41b16287db2aad0c9f4781c69d5aaf531
Tomkoid <tomaszierl@outlook.com>
2025-02-03 17:40:52 +0100

    scripts: init mic mute
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

---
+++ Scripts/mic-mute.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+pactl set-source-mute @DEFAULT_SOURCE@ toggle
+
+muted=$(pactl get-source-mute @DEFAULT_SOURCE@ toggle | awk '{print $2}')
+if [[ $muted == "yes" ]]
+then
+  notify-send -i /usr/share/icons/Papirus-Dark/32x32/devices/audio-input-microphone.svg "Mic" "Muted"
+elif [[ $muted == "no" ]]; then
+  notify-send -i /usr/share/icons/Papirus-Dark/32x32/devices/audio-input-microphone.svg "Mic" "Unmuted"
+fi
+
+echo $muted
