b2e0d3cec6bee12a44b60be581ea06ef63ab4219
Tomkoid <tomaszierl@outlook.com>
2025-11-22 17:29:07 +0100

    scripts(wayscr): update script
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- Scripts/wayscr.sh
+++ Scripts/wayscr.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+
+set -euo pipefail
+
 # kitty -e "Scripts/wayscr-prompt.sh"
 #
-# screenshot_type=$(cat /tmp/screenshot_type)
+screenshot_type=$1
 
 # grim -g "$(slurp)" - | /usr/bin/wl-copy
 # grim -g $(slurp) - | wl-copy
-grim -g "$(slurp)" - | swappy -f -
-# hyprshot -m ${screenshot_type} -r | wl-copy
+# grim -g "$(slurp)" - | swappy -f -
+hyprshot -m ${screenshot_type} --freeze --raw - | wl-copy
