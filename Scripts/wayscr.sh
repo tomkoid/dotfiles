d8519416365d2086b1a2259ebe3c077481547f36
Tomkoid <tomaszierl@outlook.com>
2025-12-04 17:30:45 +0100

    scripts(wayscr): use grimblast instead of flameshot
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- Scripts/wayscr.sh
+++ Scripts/wayscr.sh
@@ -5,9 +5,10 @@ set -euo pipefail
 
 # kitty -e "Scripts/wayscr-prompt.sh"
 #
-screenshot_type=$1
+# screenshot_type=$1
 
 # grim -g "$(slurp)" - | /usr/bin/wl-copy
 # grim -g $(slurp) - | wl-copy
 # grim -g "$(slurp)" - | swappy -f -
-hyprshot -m ${screenshot_type} --freeze --raw - | wl-copy
+# hyprshot -m ${screenshot_type} --freeze --raw - | wl-copy
+grimblast --cursor --wait 1 --freeze save area - | swappy -f -
