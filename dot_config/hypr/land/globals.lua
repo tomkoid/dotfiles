422322e7787bd835fa0d2bdc6f5fd9bc8ac9e3f9
Tomkoid <tomkoid@tomkoid.cz>
2026-09-23 20:25:00 +0200

    ghostty instead of kitty

--- dot_config/hypr/land/globals.lua
+++ dot_config/hypr/land/globals.lua
@@ -3,7 +3,8 @@
 ---------------------
 
 -- Set programs that you use
-TERMINAL = "kitty --single-instance"
+-- TERMINAL = "kitty --single-instance"
+TERMINAL = "ghostty +new-window"
 FILEMANAGER = "dolphin"
 WEBBROWSER = "zen-browser"
 MENU = "fuzzel"
